47ec95b0c5cec41563c830038adc0330cc6ddc73
Billyas <billy@jmu.edu.cn>
2021-05-15 23:25:28 +0800

    h

---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rc4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String a = textBox1.Text;
+            String key = textBox2.Text;
+            //String b = CryptConfig(a, key);
+            String mi = rc4.RC4.Encrypt(key, a);
+            textBox3.Text = mi;
+            //MessageBox.Show("密文是::"+mi,"提示");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String a = textBox1.Text;
+            String key = textBox2.Text;
+            //String b = CryptConfig(a, key);
+            String mi = rc4.RC4.Encrypt(key, a);
+            String res = rc4.RC4.Decrypt(key, mi);
+            //MessageBox.Show("原文是::" + res, "提示");
+            textBox4.Text = res;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    }
+
+
+    public static class RC4
+    {
+        public static string Encrypt(string key, string data)
+        {
+            Encoding unicode = Encoding.Unicode;
+
+            return Convert.ToBase64String(Encrypt(unicode.GetBytes(key), unicode.GetBytes(data)));
+        }
+
+        public static string Decrypt(string key, string data)
+        {
+            Encoding unicode = Encoding.Unicode;
+
+            return unicode.GetString(Encrypt(unicode.GetBytes(key), Convert.FromBase64String(data)));
+        }
+
+        public static byte[] Encrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+
+        public static byte[] Decrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+
+        private static byte[] EncryptInitalize(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                j = (j + key[i % key.Length] + s[i]) & 255;
+
+                Swap(s, i, j);
+            }
+
+            return s;
+        }
+
+        private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+        {
+            byte[] s = EncryptInitalize(key);
+
+            int i = 0;
+            int j = 0;
+
+            return data.Select((b) =>
+            {
+                i = (i + 1) & 255;
+                j = (j + s[i]) & 255;
+
+                Swap(s, i, j);
+
+                return (byte)(b ^ s[(s[i] + s[j]) & 255]);
+            });
+        }
+
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte c = s[i];
+
+            s[i] = s[j];
+            s[j] = c;
+        }
+    }
+}
